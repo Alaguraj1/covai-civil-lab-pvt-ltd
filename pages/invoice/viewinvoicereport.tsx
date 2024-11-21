b3b8d5e84f0e9b55c3fc5386935723242d3e1cfd
Alaguraj1 <alaguraj259@gmail.com>
2024-11-21 10:56:15 +0530

    Differentiate between the old invoice and the new invoice format.

--- pages/invoice/viewinvoicereport.tsx
+++ pages/invoice/viewinvoicereport.tsx
@@ -69,7 +69,236 @@ const Preview = () => {
 
     return (
         <>
-            <div>
+        {
+            printData?.invoice?.is_old_invoice_format ? (
+
+                <div>
+                <style
+                    type="text/css"
+                    dangerouslySetInnerHTML={{
+                        __html: '\n.style3 {\n\tfont-size: 22px;\n\tfont-weight: bold;\n}\n\ntable td, th {\n\tfont-size: 13px;\n}\n',
+                    }}
+                />
+                <div className="container" style={{ padding: '10px 0px' }}>
+                    <div className="panel">
+                        <div className="flex grid-cols-3  justify-between gap-4">
+                            <div>
+                                <div className="invoice-head text-3xl font-semibold uppercase">Invoice</div>
+                                <p className="invoice-number">Invoice No : {printData?.invoice?.invoice_no}</p>
+                            </div>
+
+                            <div className="grid-cols-9 pl-7 ltr:text-right rtl:text-left">
+                                <div className="shrink-0" style={{ display: 'flex', justifyContent: 'end' }}>
+                                    <img src="/assets/images/old-logo.png" alt="img" style={{ width: '75%' }} className="w-17 ltr:ml-auto rtl:mr-auto" />
+                                </div>
+                                <div className="mt-0 space-y-1 text-right text-white-dark">
+                                    <div className="invoice-right">
+                                        <b>An ISO/IEC 17025:2017 CERTIFIED LAB</b>
+                                        <br></br>
+                                        411/4, Ballu Naidu Thottam,Vijayalakshmi Nagar,<br></br>
+                                        Neelikonampalayam Po, Coimbatore - 6410333.<br></br>
+                                        <b>GSTIN : 33AATFC1831C1ZT</b>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <hr className="my-2 border-white-light dark:border-[#1b2e4b]" />
+                        <div className="preview-header ">
+                            <div className="mb-3 pr-3 sm:w-full md:w-1/2 lg:w-1/2 ">
+                                <div className="space-y-1 text-white-dark">
+                                    <div>Issued For:</div>
+                                    <div className="font-semibold text-black dark:text-white">
+                                        {printData?.customer?.customer_name} <br />
+                                        {printData?.customer?.address1}
+                                        <br></br>
+                                        <div className="space-y-1 text-white-dark">
+                                            -Original for Recipient<br></br>
+                                            -Duplicate for Supplier Transporter<br></br>
+                                            -Triplicate for Supplier
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="issue_date  mt-3 sm:flex-row  md:w-1/2  lg:w-1/2">
+                                <div className=" ">
+                                    <div className="mb-1 flex w-full items-center justify-between">
+                                        <div className="text-white-dark">Date:</div>
+                                        <div>{printData?.invoice?.date}</div>
+                                    </div>
+
+                                    <div className="mb-1 flex w-full items-center justify-between">
+                                        <div className="text-white-dark">Project Name:</div>
+                                        <div>{printData?.invoice?.project_name}</div>
+                                    </div>
+                                    <div className="mb-1 flex w-full items-center justify-between">
+                                        <div className="text-white-dark">Place of Testing:</div>
+                                        <div>{printData?.invoice?.place_of_testing}</div>
+                                    </div>
+                                    <div className="mb-1 flex w-full items-center justify-between">
+                                        <div className="text-white-dark"> GSTIN/UIN:</div>
+                                        <div>{printData?.customer?.gstin_no}</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="table-responsive invoice-table mt-3">
+                            <table className="table-striped">
+                                <thead style={{ border: '1px solid black' }}>
+                                    <tr>
+                                        <th style={{ border: '1px solid black' }}>S.No</th>
+                                        <th style={{ border: '1px solid black' }}>Name of Test</th>
+                                        <th style={{ border: '1px solid black' }}>HAN/SAC</th>
+                                        <th style={{ border: '1px solid black' }}>Qty</th>
+                                        <th style={{ textAlign: 'right', border: '1px solid black' }}>Rate/Sample(INR)</th>
+                                        <th style={{ textAlign: 'right', border: '1px solid black' }}>Amount(INR)</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {printData?.invoice_tests?.map((invoice: any, index: any) => {
+                                        return (
+                                            <>
+                                                <tr style={{ border: '1px solid black' }}>
+                                                    <td style={{ border: '1px solid black' }}>{index + 1}</td>
+                                                    <td style={{ border: '1px solid black' }}>
+                                                        {invoice?.test_name} - <span style={{ fontWeight: 'bold' }}>{invoice?.material_name}</span>
+                                                    </td>
+                                                    <td style={{ border: '1px solid black' }}>998346</td>
+                                                    <td style={{ border: '1px solid black' }}>{invoice?.qty}</td>
+                                                    <td style={{ textAlign: 'right', border: '1px solid black' }}>{invoice?.price_per_sample}</td>
+                                                    <td style={{ textAlign: 'right', border: '1px solid black' }}>{parseInt(invoice?.total, 10)}</td>
+                                                </tr>
+                                            </>
+                                        );
+                                    })}
+                                </tbody>
+                            </table>
+                            <table>
+                                <thead></thead>
+                                <tbody>
+                                    <tr style={{ border: 'none' }}>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                    <tr style={{ border: 'none' }}>
+                                        <>
+                                            <td>
+                                                <b>Name:</b> Covai Civil Tech Lab
+                                            </td>
+
+                                            {printData?.invoice?.discount >= 1 ? (
+                                                <>
+                                                    <td style={{ textAlign: 'right' }}>Discount (%) </td>
+                                                    <td style={{ textAlign: 'right' }}>{printData?.invoice?.discount}</td>
+                                                </>
+                                            ) : null}
+                                        </>
+                                    </tr>
+                                    <tr style={{ border: 'none' }}>
+                                        <>
+                                            <td>
+                                                <b>Account Number:</b> 584705000004{' '}
+                                            </td>
+
+                                            <td style={{ textAlign: 'right' }}>Before Tax</td>
+                                            <td style={{ textAlign: 'right' }}>{BeforeTotal?.toFixed(2)}</td>
+
+                                            {/* <td style={{ textAlign: 'right' }}>TDS Amount</td>
+                                            <td style={{ textAlign: 'right' }}>{printData?.invoice?.tds_amount}</td> */}
+                                        </>
+                                    </tr>
+
+                                    <tr style={{ border: 'none' }}>
+                                        <td>
+                                            <b>Branch:</b> Saibaba colony branch, coimbatore.
+                                        </td>
+
+                                        <td style={{ textAlign: 'right' }}>{Discount()}</td>
+                                        <td style={{ textAlign: 'right' }}>{Tax_total?.toFixed(2)}</td>
+                                    </tr>
+
+                                    <tr style={{ border: 'none' }}>
+                                        <td>
+                                            <b>IFSC Code:</b> ICIC0001550
+                                        </td>
+
+                                        <td style={{ textAlign: 'right' }}>Total</td>
+                                        <td style={{ textAlign: 'right', fontWeight: 'bold' }}>
+                                            {parseInt(After_Tax)} <input type="hidden" id="amt" name="amt" defaultValue="11,446.00" />
+                                        </td>
+                                    </tr>
+
+                                    {/* <tr style={{ border: 'none' }}></tr> */}
+                                    {/* <tr style={{ border: 'none' }}>
+                                        <td>{null}</td>
+
+                                        
+                                    </tr> */}
+                                </tbody>
+                            </table>
+                        </div>
+
+                        {/*footer */}
+                        <hr className="my-2 border-white-light dark:border-[#1b2e4b]" />
+                        <div className="preview-footer-main">
+                            <div className="preview-qr-outer1">
+                                <img src="/assets/images/Sponsor.jpg" className="preview-qr-outer1_img" alt="image" />
+                            </div>
+
+                            {printData?.invoice?.invoice_no === null ? (
+                                <></>
+                            ) : (
+                                <>
+                                    <div className="preview-qr-outer2">
+                                        <img src={printData?.invoice?.qr} className="preview-qr-outer2_img" alt="image" />
+                                    </div>
+                                </>
+                            )}
+
+                            <div className="preview-qr-outer3">
+                                <div className="sign-footer mt-0 grid-cols-9 space-y-1 text-right text-right text-sm text-white-dark">
+                                    <img src="/assets/images/sign.png" alt="img" style={{ marginLeft: 'auto' }} />
+                                    <br />
+                                    COVAI CIVIL TECH LAB <br /> R.TIRUMALAI (TECHNICAL DIRECTOR)
+                                    {/* <img src="/assets/images/logo_3.jpg" alt="img" style={{ marginLeft: "auto" }} /> */}
+                                    <div className="sign-footer mt-0 space-y-1 text-right text-sm text-white-dark">
+                                        <b>Phone</b> : <a href="tel:9840014193"> 9840014193 </a>|<br />
+                                        <i>
+                                            <b>Email :</b>{' '}
+                                            <a href="mailto:cbe@covaiciviltechlab.com" target="_blank">
+                                                cbe@covaiciviltechlab.com{' '}
+                                            </a>
+                                        </i>{' '}
+                                        <br />
+                                        <i>
+                                            <b>Website :</b>{' '}
+                                            <a href="https://covaiciviltechlab.com/" target="blank">
+                                                covaiciviltechlab.com
+                                            </a>
+                                        </i>
+                                        <br></br>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="footer-decleration container">
+                    {' '}
+                    <div style={{ textAlign: 'center', padding: '10px 10px 0px 10px' }}>
+                        <b>Declaration:-</b> We declare that this invoice shows the actual price of the Test Services described and that all particulars are true and correct. <br /> <br />
+                    </div>
+                    <div style={{ textAlign: 'center' }}>
+                        SUBJECT TO COIMBATORE JURISDICTION
+                        <br /> This is computer Generated Invoice.
+                    </div>
+                </div>
+            </div>
+            ) : (
+                <div>
                 <style
                     type="text/css"
                     dangerouslySetInnerHTML={{
@@ -298,6 +527,11 @@ const Preview = () => {
                     </div>
                 </div>
             </div>
+            )
+        }
+         
+
+          
         </>
     );
 };
